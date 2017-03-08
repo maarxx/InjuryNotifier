8b31d252780fcf2d49fbfeb2115b6ddcd7264764
maarxx <chris.lewark@gmail.com>
2017-03-08 01:43:40 -0500

    Removed test Log.Message

--- InjuryNotifier/InjuryNotifier.cs
+++ InjuryNotifier/InjuryNotifier.cs
@@ -10,11 +10,13 @@ namespace InjuryNotifier
 
         public InjuryNotifier(Map map) : base(map)
         {
+            //Log.Message("Hello from InjuryNotifier InjuryNotifier");
             updateCollection(true);
         }
 
         public override void MapComponentTick()
         {
+            //Log.Message("Hello from InjuryNotifier MapComponentTick");
             slowDown++;
             if (slowDown > 100)
             {
@@ -25,10 +27,12 @@ namespace InjuryNotifier
 
         public void updateCollection(bool notify)
         {
+            //Log.Message("Hello from InjuryNotifier updateCollection BEGIN");
             foreach (Pawn p in map.mapPawns.FreeColonistsAndPrisonersSpawned)
             {
                 foreach (Hediff_MissingPart hmp in p.health.hediffSet.GetMissingPartsCommonAncestors())
                 {
+                    //Log.Message(p.NameStringShort + " lost " + hmp.Part.def.label + " due to " + hmp.Label + "!");
                     bool isNew = false;
                     isNew = set.Add(new PawnPartProblem(p.thingIDNumber, hmp.Part.def.label, hmp.Label));
                     if (notify && isNew)
@@ -43,6 +47,7 @@ namespace InjuryNotifier
                         Hediff_Injury hi = (Hediff_Injury)h;
                         if (hi.IsOld())
                         {
+                            //Log.Message(p.NameStringShort + " has new " + hi.Label + " on their " + hi.Part.def.label + "!");
                             bool isNew = false;
                             isNew = set.Add(new PawnPartProblem(p.thingIDNumber, hi.Part.def.label, hi.Label));
                             if (notify && isNew)
@@ -53,6 +58,7 @@ namespace InjuryNotifier
                     }
                 }
             }
+            //Log.Message("Hello from InjuryNotifier updateCollection END");
         }
     }
 
