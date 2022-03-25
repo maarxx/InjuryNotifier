0f048ae876e379af1650314baecb5f5513aa9333
Mlie <emipa606@users.noreply.github.com>
2022-03-25 22:22:41 +0100

    Updated to 1.3

---
+++ Source/InjuryNotifier/MapComponent_InjuryNotifier.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace InjuryNotifier;
+
+public class MapComponent_InjuryNotifier : MapComponent
+{
+    public bool firstRun = true;
+    public HashSet<PawnPartProblem> set = new HashSet<PawnPartProblem>();
+    public int slowDown;
+
+    public MapComponent_InjuryNotifier(Map map) : base(map)
+    {
+        LongEventHandler.QueueLongEvent(ensureComponentExists, null, false, null);
+    }
+
+    public static void ensureComponentExists()
+    {
+        foreach (var m in Find.Maps)
+        {
+            if (m.GetComponent<MapComponent_InjuryNotifier>() == null)
+            {
+                m.components.Add(new MapComponent_InjuryNotifier(m));
+            }
+        }
+    }
+
+    public override void MapComponentTick()
+    {
+        slowDown++;
+        if (slowDown <= 100)
+        {
+            return;
+        }
+
+        slowDown = 0;
+        if (firstRun)
+        {
+            updateCollection(false);
+            firstRun = false;
+        }
+        else
+        {
+            updateCollection(true);
+        }
+    }
+
+    public void updateCollection(bool notify)
+    {
+        //Log.Message("Hello from InjuryNotifier updateCollection BEGIN");
+        foreach (var p in map.mapPawns.FreeColonistsAndPrisonersSpawned)
+        {
+            foreach (var hmp in p.health.hediffSet.GetMissingPartsCommonAncestors())
+            {
+                //Log.Message(p.NameStringShort + " lost " + hmp.Part.def.label + " due to " + hmp.Label + "!");
+                var isNew = set.Add(
+                    new PawnPartProblem(p.thingIDNumber, hmp.Part.def.label, trimInjuryLabel(hmp.Label)));
+                if (notify && isNew)
+                {
+                    //Log.Message(p.NameStringShort + " lost " + hmp.Part.def.label + " due to " + hmp.Label + "!");
+                    Find.LetterStack.ReceiveLetter("Lost Part",
+                        p.Name.ToStringShort + " lost their " + hmp.Part.def.label + " due to " +
+                        trimInjuryLabel(hmp.Label) + "!", LetterDefOf.NegativeEvent, new GlobalTargetInfo(p));
+                }
+            }
+
+            foreach (var h in p.health.hediffSet.hediffs)
+            {
+                if (h is not Hediff_Injury hi)
+                {
+                    continue;
+                }
+
+                if (!hi.IsPermanent())
+                {
+                    continue;
+                }
+
+                //Log.Message(p.NameStringShort + " has new " + hi.Label + " on their " + hi.Part.def.label + "!");
+                var isNew = set.Add(new PawnPartProblem(p.thingIDNumber, hi.Part.def.label,
+                    trimInjuryLabel(hi.Label)));
+                if (notify && isNew)
+                {
+                    //Log.Message(p.NameStringShort + " has new " + hi.Label + " on their " + hi.Part.def.label + "!");
+                    Find.LetterStack.ReceiveLetter("New Scar",
+                        p.Name.ToStringShort + " has new " + trimInjuryLabel(hi.Label) + " on their " +
+                        hi.Part.def.label + "!", LetterDefOf.NegativeEvent, new GlobalTargetInfo(p));
+                }
+            }
+        }
+        //Log.Message("Hello from InjuryNotifier updateCollection END");
+    }
+
+    public static string trimInjuryLabel(string s)
+    {
+        var leftParenIndex = s.IndexOf(" (");
+        if (leftParenIndex > 0)
+        {
+            return s.Substring(0, leftParenIndex);
+        }
+
+        return s;
+    }
+}
---
+++ Source/InjuryNotifier/PawnPartProblem.cs
@@ -0,0 +1,30 @@
+﻿namespace InjuryNotifier;
+
+public struct PawnPartProblem
+{
+    public readonly int pawnIDNumber;
+    public readonly string part_label;
+    public readonly string hediff_label;
+
+    public PawnPartProblem(int pawnIDNumber, string part_label, string hediff_label)
+    {
+        this.pawnIDNumber = pawnIDNumber;
+        this.part_label = part_label;
+        this.hediff_label = hediff_label;
+    }
+
+    public override int GetHashCode()
+    {
+        return pawnIDNumber.GetHashCode() * part_label.GetHashCode() * hediff_label.GetHashCode();
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is PawnPartProblem problem && Equals(problem);
+    }
+
+    public bool Equals(PawnPartProblem ppp)
+    {
+        return ppp.pawnIDNumber == pawnIDNumber && ppp.part_label == part_label && ppp.hediff_label == hediff_label;
+    }
+}
